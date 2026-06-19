640d5861667d25dbf480f353b84056dc4a3c677d
StarBot-Git <starbot.allstar.co@gmail.com>
2026-06-19 11:52:56 -0500

    Allow current Azure frontend origin

--- services/api-service/src/core/config.py
+++ services/api-service/src/core/config.py
@@ -45,6 +45,7 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://purple-water-03112dc0f.1.azurestaticapps.net",
+        "https://purple-water-03112dc0f.4.azurestaticapps.net",
         "https://myallstarplatform.com",
         "https://www.myallstarplatform.com",
         "https://api-vm.tail6cef8e.ts.net",
